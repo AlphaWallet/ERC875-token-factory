bf681331432186083c1a6c4565d61edea33cb639
James Sangalli <j.l.sangalli@gmail.com>
2019-01-31 14:43:56 +1100

    with new bytecode and abi for latest contract

--- abi.js
+++ abi.js
@@ -351,20 +351,6 @@ let abi = [
     {
         "constant": true,
         "inputs": [],
-        "name": "getAmountTransferred",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [],
         "name": "street",
         "outputs": [
             {
